493347dfd7ed3f5d50a4e0d0ce9ca58f65425b9c
Edward Ward <edwardwardward@gmail.com>
2017-07-11 15:36:58 +0100

    add trading pair class

--- public_data/models.py
+++ public_data/models.py
@@ -4,7 +4,20 @@ import gdax
 
 public_client = gdax.PublicClient()
 
-class EthBtc(models.Model):
-    product = models.CharField(max_length=200)
-    ticker = models.CharField(
-        max_length=3)
+#Class to model a ONE/TWO pair e.g. ETH/BTC market price ≈ 0.1
+class TradingPair(models.Model):
+    title = models.CharField(max_length=200)
+    bid = models.FloatField()
+    ask = models.FloatField()
+    one_name = models.CharField(max_length=3)
+    one_balance = models.FloatField()
+    two_name = models.CharField(max_length=3)
+    two_balance = models.FloatField()
+
+    def add_trading_pair(self,one,two):
+        self.title = (one + '-' + two)
+        self.one_name = one
+        self.two_name = two
+        order_book = public_client.get_product_order_book(self.title)
+        self.bid = order_book['bids'][0][0]
+        self.ask = order_book['asks'][0][0]
